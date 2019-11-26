5eec64e8d280bf6ac67da2b46affcafd1ef9bc45
Florin Gogianu <florin.gogianu@gmail.com>
2019-11-26 18:37:27 +0200

    Fix twitter card image link.

--- src/components/Head.js
+++ src/components/Head.js
@@ -30,18 +30,18 @@ export default ({
     {description && <meta property="og:description" content={description} />}
     {siteTitle && <meta property="og:site_name" content={siteTitle} />}
     {tags && tags.slice(0, 6).map(tag => <meta key={tag} property="article:tag" content={tag} />)}
-    {image && <meta property="og:image" content={image} />}
+    {image && <meta property="og:image" content={`${siteTitle}${image}`} />}
 
     {/* Twitter Cards */}
     <meta name="twitter:card" content="summary" />
     {siteTwitter && <meta name="twitter:site" content={siteTwitter} />}
     {title && <meta name="twitter:title" content={title} />}
     {description && <meta name="twitter:description" content={description} />}
-    {image && <meta name="twitter:image" content={image} />}
+    {image && <meta name="twitter:image" content={`${siteTitle}${image}`} />}
 
     {/* Schema.org markup */}
     {description && <meta itemProp="description" content={description} />}
     {tags && <meta itemProp="keywords" content={tags.join(',')} />}
-    {image && <meta itemProp="image" content={image} />}
+    {image && <meta itemProp="image" content={`${siteTitle}${image}`} />}
   </Head>
 )
